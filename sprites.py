7e5a2d65815d65c75cb4823ec8b003244857d984
Vanja Obradovic <72192629+vanja-obradovic@users.noreply.github.com>
2021-11-11 10:41:42 +0100

    Final

--- sprites.py
+++ sprites.py
@@ -4,7 +4,8 @@ import config
 import time
 
 
-def check_bounds(row, col, game_map: list):
+# ------------------------- GENERAL PURPOSE FUNCTIONS ------------------------- #
+def check_bounds(row, col, game_map: list):  # used by dfs, bbs, a*
     options = []
     if col - 1 >= 0:
         options.append([row, col - 1])
@@ -17,19 +18,77 @@ def check_bounds(row, col, game_map: list):
     return options
 
 
-def check_costs(game_map):
-    row_length = len(game_map)
-    col_length = len(game_map[0])
-    min = 9999999
-    for i in range(row_length):
-        for j in range(col_length):
-            cost = game_map[i][j].cost()
-            if min > cost: min = cost
-    return min
+def list_refresh(current: list, update: list):  # used by bbs, a*
+    len1, len2 = len(update), len(current)
+    i, j = 0, 0
+    while i < len1:
+        cost, last_node = update[i][1][0], update[i][1][1]
+        while j < len2:
+            if last_node == current[j][1][1]:
+                if cost < current[j][1][0]:
+                    current.pop(j)
+                    len2 -= 1
+                    j -= 1
+                else:
+                    # if partial path to be added, has >= cost than partial path with same last node skip it
+                    update.pop(i)
+                    i -= 1
+                    len1 -= 1
+                    break
+            j += 1
+        j = 0
+        i += 1
+    current.extend(update)
 
-# [[ [[coor]], cost ]]
+    return current
 
-def check_bounds_bbs(partial_path: list, game_map: list, goal: list):
+
+# ------------------------- DFS FUNCTIONS ------------------------- #
+def choose_best(options: list, game_map: list) -> list:  # used by dfs
+    options.sort(key=lambda elem: game_map[elem[0]][elem[1]].cost(), reverse=True)
+    return options
+
+
+def compute_possible_jump(row, col, game_map: list, path: list):  # used by dfs
+    options = check_bounds(row, col, game_map)
+    tmp = [elem for elem in options if game_map[elem[0]][elem[1]] not in path]
+    return tmp
+
+
+def check_if_neighbours(a: list, b: list):  # used by dfs
+    if (abs(a[0] - b[0]) == 1 and a[1] == b[1]) or (abs(a[1] - b[1]) == 1 and a[0] == b[0]):
+        return True
+    else:
+        return False
+
+
+# ------------------------- BFS FUNCTIONS ------------------------- #
+def compute_traversal_score(target: list, game_map: list, position: list) -> int:  # used by bfs
+    tmp = check_bounds(target[0], target[1], game_map)
+    options = [elem for elem in tmp if elem != position]
+    result = 0
+    for opt in options:
+        result += game_map[opt[0]][opt[1]].cost()
+    return result / len(options)
+
+
+def check_traversal(row, col, game_map: list) -> list:  # used by bfs
+    options = []
+    if row - 1 >= 0:
+        options.append([row - 1, col])
+    if col + 1 < len(game_map[row]):
+        options.append([row, col + 1])
+    if row + 1 < len(game_map):
+        options.append([row + 1, col])
+    if col - 1 >= 0:
+        options.append([row, col - 1])
+
+    options.sort(key=lambda elem: compute_traversal_score(elem, game_map, [row, col]))
+    return options
+
+
+# ------------------------- BBS FUNCTIONS ------------------------- #
+def check_bounds_bbs(partial_path: list, game_map: list, goal: list):  # used by bbs
     curr_path: list = list(partial_path[0])
     cost, last_node = partial_path[1][0], partial_path[1][1]
 
@@ -47,31 +106,19 @@ def check_bounds_bbs(partial_path: list, game_map: list, goal: list):
     return new_paths, False
 
 
-def list_refresh(current: list, update: list):
-    len1, len2 = len(update), len(current)
-    i, j = 0, 0
-    while i < len1:
-        cost, last_node = update[i][1][0], update[i][1][1]
-        while j < len2:
-            if last_node == current[j][1][1]:
-                if cost < current[j][1][0]:
-                    current.pop(j)
-                    len2 -= 1
-                    j -= 1
-                # elif cost > current[j][1][0]:
-                else:
-                    update.pop(i)
-                    len1 -= 1
-                    i -= 1
-                    break
-            j += 1
-        j = 0
-        i += 1
-    current.extend(update)
-    return current
+# ------------------------- A* FUNCTIONS ------------------------- #
+def check_costs(game_map):  # used by a*
+    row_length = len(game_map)
+    col_length = len(game_map[0])
+    min = 9999999
+    for i in range(row_length):
+        for j in range(col_length):
+            cost = game_map[i][j].cost()
+            if min > cost: min = cost
+    return min
 
 
-def a_star_heuristics(candidate: list, goal: list, min_tile):
+def a_star_heuristics(candidate: list, goal: list, min_tile):  # used by a*
     if candidate == goal:
         return 0
     edge1, edge2 = abs(candidate[0] - goal[0]) + 1, abs(candidate[1] - goal[1]) + 1
@@ -82,7 +129,7 @@ def a_star_heuristics(candidate: list, goal: list, min_tile):
     return approx_path_cost
 
 
-def check_bounds_a_star(partial_path: list, game_map: list, goal: list, min_tile):
+def check_bounds_a_star(partial_path: list, game_map: list, goal: list, min_tile):  # used by a*
     curr_path: list = list(partial_path[0])
     cost, last_node = partial_path[1][0], partial_path[1][1]
 
@@ -103,43 +150,6 @@ def check_bounds_a_star(partial_path: list, game_map: list, goal: list, min_tile
             [curr_path + [opt], [tmp, opt,
                                  tmp + a_star_heuristics(opt, goal, min_tile)]])
     return new_paths, False
-
-
-def compute_possible_jump(row, col, game_map: list, path: list):
-    options = check_bounds(row, col, game_map)
-    tmp = [elem for elem in options if game_map[elem[0]][elem[1]] not in path]
-    return tmp
-
-
-def check_if_neighbours(a: list, b: list):
-    if (abs(a[0] - b[0]) == 1 and a[1] == b[1]) or (abs(a[1] - b[1]) == 1 and a[0] == b[0]):
-        return True
-    else:
-        return False
-
-
-def compute_traversal_score(target: list, game_map: list, position: list) -> int:
-    tmp = check_bounds(target[0], target[1], game_map)
-    options = [elem for elem in tmp if elem != position]
-    result = 0
-    for opt in options:
-        result += game_map[opt[0]][opt[1]].cost()
-    return result / len(options)
-
-
-def check_traversal(row, col, game_map: list) -> list:
-    options = []
-    if row - 1 >= 0:
-        options.append([row - 1, col])
-    if col + 1 < len(game_map[row]):
-        options.append([row, col + 1])
-    if row + 1 < len(game_map):
-        options.append([row + 1, col])
-    if col - 1 >= 0:
-        options.append([row, col - 1])
-
-    options.sort(key=lambda elem: compute_traversal_score(elem, game_map, [row, col]))
-    return options
 
 
 class BaseSprite(pygame.sprite.Sprite):
@@ -209,30 +219,36 @@ class Aki(Agent):
     def __init__(self, row, col, file_name):
         super().__init__(row, col, file_name)
 
-    def __choose_best(self, options: list, game_map: list) -> list:
-        options.sort(key=lambda elem: game_map[elem[0]][elem[1]].cost(), reverse=True)
-        return options
-
     def get_agent_path(self, game_map, goal):
         path = [game_map[self.row][self.col]]
 
         dfs_list = []
         row = self.row
         col = self.col
+
+        start = time.time()
+
         while True:
             options = compute_possible_jump(row, col, game_map, path)
-            dfs_list.extend(Aki.__choose_best(self, options, game_map))
+            dfs_list.extend(choose_best(options, game_map))
             row, col = dfs_list.pop()
+
             if len(options) == 0:
                 while True:
-                    if not check_if_neighbours(list(path[-1].position()), [row, col]):
+                    if not check_if_neighbours(list(path[-1].position()), [row, col]):  # backtracking if stuck
                         path.pop()
                     else:
                         break
+
             if row == goal[0] and col == goal[1]:
                 path.append(game_map[row][col])
                 break
+
             path.append(game_map[row][col])
+
+        end = time.time()
+        print(end - start)
+
         return path
 
 
@@ -242,29 +258,38 @@ class Jocke(Agent):
 
     def get_agent_path(self, game_map, goal):
         path = []
+        bfs_graph = {}
+        curr_pop_list = []
+        cnt = 0
+
         row = self.row
         col = self.col
         start = game_map[row][col]
-        bfs_graph = {}
-        curr_pop_list = []
+
+        start_time = time.time()
+
         while True:
             options = check_traversal(row, col, game_map)
             bfs_graph[(row, col)] = options
-            if len(bfs_graph) == 1:
-                iterator = next(iter(bfs_graph))
-                cnt = 1
-                curr_pop_list = bfs_graph.get(iterator)
+
             if [goal[0], goal[1]] in options:
                 path.append(game_map[goal[0]][goal[1]])
                 break
-            if len(curr_pop_list) == 0:
+
+            if len(bfs_graph) == 1:  # starting state, get first list to process
+                iterator = next(iter(bfs_graph))
+                cnt = 1
+                curr_pop_list = bfs_graph.get(iterator)
+
+            if len(curr_pop_list) == 0:  # when curr list empty, find next to process
                 iterator = iter(bfs_graph)
                 tmp = ()
                 for i in range(cnt):
                     next(iterator)
                 tmp = next(iterator)
-                cnt += 1
+                cnt += 1  # increase position in dictionary for next list
                 curr_pop_list = list(bfs_graph.get(tmp))
+
             row, col = curr_pop_list.pop(0)
 
         keys = list(bfs_graph.keys())
@@ -280,6 +305,10 @@ class Jocke(Agent):
                     break
             curr = list(keys[index])
             path.insert(0, game_map[curr[0]][curr[1]])
+
+        end = time.time()
+        print(end - start_time)
+
         return path
 
 
@@ -290,7 +319,9 @@ class Draza(Agent):
     def get_agent_path(self, game_map, goal):
         row = self.row
         col = self.col
+
         path = []
+
         bbs_list = [[[[row, col]], [0, [row, col]]]]
         start = time.time()
         while True:
@@ -298,13 +329,15 @@ class Draza(Agent):
             new_paths, end = check_bounds_bbs(partial_path, game_map, list(goal))
             if not end:
                 bbs_list = list_refresh(bbs_list, new_paths)
-                bbs_list.sort(key=lambda elem: (elem[1][0], len(elem[0])))
-            else:
+                bbs_list.sort(key=lambda elem: (elem[1][0], len(elem[0])))  # sort by price,
+            else:  # then by num of nodes in partial path
                 for elem in new_paths[0][0]:
                     path.append(game_map[elem[0]][elem[1]])
                 break
+
         end = time.time()
         print(end - start)
+
         return path
 
 
@@ -315,22 +348,28 @@ class Bole(Agent):
     def get_agent_path(self, game_map, goal):
         row = self.row
         col = self.col
+
         path = []
         a_star_list = [[[[row, col]], [0, [row, col], 0]]]
         min_tile = check_costs(game_map)
+
         start = time.time()
         while True:
             partial_path = a_star_list.pop(0)
             new_paths, end = check_bounds_a_star(partial_path, game_map, list(goal), min_tile)
             if not end:
                 a_star_list = list_refresh(a_star_list, new_paths)
-                a_star_list.sort(key=lambda elem: (elem[1][2], len(elem[0])))
+                a_star_list.sort(key=lambda elem: (
+                    elem[1][2],
+                    len(elem[0])))  # sort by sum of price and heuristic, then by num of nodes in partial path
             else:
                 for elem in new_paths[0][0]:
                     path.append(game_map[elem[0]][elem[1]])
                 break
+
         end = time.time()
         print(end - start)
+
         return path
 
 
